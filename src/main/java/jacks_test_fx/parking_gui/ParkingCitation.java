7fbe1d3c14ba08f2f9559c386caa9a9dd50b9477
Gabe Allen <gabeallen90@gmail.com>
2026-03-27 18:57:11 -0600

    Refactored Car.java & ParkingCitation.java, mostly just added doc comments, also slightly altered variable names.

--- src/main/java/jacks_test_fx/parking_gui/ParkingCitation.java
+++ src/main/java/jacks_test_fx/parking_gui/ParkingCitation.java
@@ -8,13 +8,15 @@ import java.time.LocalDate;
  * This class represents a parking citation object with a basic constructor and some getter methods.
  */
 public class ParkingCitation {
-	private double feeAmnt = 15.00;
+	private double feeAmount = 15.00;
 	private LocalDate dateCited, payDeadline;
 	private Car carCited;
 	private User customerCited;
 	private String citationNotes;
 	
 	
+	//Constructors
+	//========================================================================
 	/**
 	 * Constructor takes a car object and derives other fields automatically.
 	 * dateCited gets the current date.
@@ -45,11 +47,36 @@ public class ParkingCitation {
 	}
 	
 	//Getters
-	//=========================================================================
-	public double getFeeAmnt() { return feeAmnt; }
+	//========================================================================
+
+	/**
+	 * Gets the fee amount for the citation.
+	 * @return a double representing the fee amount.
+	 */
+	public double getFeeAmount() { return feeAmount; }
+	/**
+	 * Gets the date the citation was given.
+	 * @return a LocalDate value representing when the citation was given.
+	 */
 	public LocalDate getDateCited() { return dateCited; }
+	/**
+	 * Gets the payment deadline for the citation.
+	 * @return a LocalDate value representing when the citation must be payed by.
+	 */
 	public LocalDate getPayDeadline() { return payDeadline; }
+	/**
+	 * Gets the car being cited in the citation.
+	 * @return a Car object representing the car in the wrong.
+	 */
 	public Car getCarCited() { return carCited; }
+	/**
+	 * Gets the owner of the car being cited.
+	 * @return a User object representing the person being cited.
+	 */
 	public User getCustomerCited() { return customerCited; }
+	/**
+	 * Gets the notes associated with the citation.
+	 * @return a String of any notes included with the citation.
+	 */
 	public String getNotes() { return citationNotes; }
 }
