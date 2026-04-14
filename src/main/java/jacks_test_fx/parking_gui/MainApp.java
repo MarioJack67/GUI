3e27174655efbbe81b5527c2ddda360d10a2354e
Gabe Allen <gabeallen90@gmail.com>
2026-04-13 19:03:45 -0600

    Worked on registration GUI briefly.

--- src/main/java/jacks_test_fx/parking_gui/MainApp.java
+++ src/main/java/jacks_test_fx/parking_gui/MainApp.java
@@ -17,7 +17,7 @@ public class MainApp extends Application {
         stage=s;
         stage.setMinWidth(650);
         stage.setMinHeight(500);
-        setRoot("citation","Parking Lot Managment System");
+        setRoot("registration","Parking Lot Managment System");
     }
 
     static void setRoot(String fxml) throws IOException {
